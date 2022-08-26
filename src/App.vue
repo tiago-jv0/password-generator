4ad1d1830f00d7b682fbbb33ad7c0087337b4f1f
Tiago Jose <tiago_jvo@outlook.com>
2022-08-26 16:19:17 -0300

    feat: initial setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <h1>Password Generator</h1>
+
+    <input type="text" value="P4$5W0rD!" />
+
+    Character Length Include Uppercase Letters Include Lowercase Letters Include
+    Numbers Include Symbols Strength Generate
+  </main>
+</template>
+
+<script setup lang="ts"></script>
